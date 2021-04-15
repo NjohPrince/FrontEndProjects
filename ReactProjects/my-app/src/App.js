f62b5bbe18ca878c33ac8106e587683cf00d0fec
NjohPrince <jufredprince@gmail.com>
2021-04-15 14:07:13 +0100

    finalized rotating effect

--- ReactProjects/my-app/src/App.js
+++ ReactProjects/my-app/src/App.js
@@ -7,15 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="card card-1">
-          <StyledCard title="Getty Images unsplash.com" type="error">
-            <img src="https://source.unsplash.com/random/800x500" alt="image-placeholder" />
-          </StyledCard>
-        </div>
-        <div className="card card-2">
-          <StyledCard title="Just unsplash.com" type="success">
-            <img src="https://source.unsplash.com/random/800x500" alt="image-placeholder" />
-          </StyledCard>
+        <div className="box-container">
+          <div className="card card-1">
+            <StyledCard title="Getty Images unsplash.com" type="error">
+              <img src="https://source.unsplash.com/random/800x500" alt="image-placeholder" />
+            </StyledCard>
+          </div>
+          <div className="card card-2">
+            <StyledCard title="Just unsplash.com" type="success">
+              <img src="https://source.unsplash.com/random/800x500" alt="image-placeholder" />
+            </StyledCard>
+          </div>
         </div>
       </div>
     );
