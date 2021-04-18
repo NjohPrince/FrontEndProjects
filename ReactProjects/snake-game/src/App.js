0feace8bd5a0e2d190fd66d2e0fe91eeea0b2e84
NjohPrince <jufredprince@gmail.com>
2021-04-18 12:08:19 +0100

    snake movement

--- ReactProjects/snake-game/src/App.js
+++ ReactProjects/snake-game/src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 
   state = {
     food: getRandomCoordinates(),
+    speed: 200,
     direction: 'RIGHT',
     snakeDots: [
       [0, 0],
@@ -22,10 +23,53 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed);
     document.onkeydown = this.onkeydown;
   }
 
-  
+  onkeydown = (e) => {
+    e = e || window.event;
+    switch(e.keyCode) {
+      case 38:
+        this.setState({direction: 'UP'});
+        break;
+      case 40:
+        this.setState({direction: 'DOWN'});
+        break;
+      case 37:
+        this.setState({direction: 'LEFT'});
+        break;
+      case 39:
+        this.setState({direction: 'RIGHT'});
+        break;
+    }
+  }
+
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots];
+    let head = dots[dots.length - 1];
+
+    switch(this.state.direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'LEFT':
+        head = [head[0] - 2, head[1]];
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    dots.push(head);
+    dots.shift();
+
+    this.setState({
+      snakeDots: dots
+    })
+  }
 
   render () {
     return (
