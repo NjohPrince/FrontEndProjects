ee69095fc8a895151e68279c36c678bbb8971fb4
NjohPrince <jufredprince@gmail.com>
2021-04-18 11:46:48 +0100

    random food position

--- ReactProjects/snake-game/src/App.js
+++ ReactProjects/snake-game/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Food from './Food';
 import Snake from './Snake'
+
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x, y];
+}
 
 class App extends React.Component {
 
   state = {
+    food: getRandomCoordinates(),
     snakeDots: [
       [0, 0],
       [2, 0]
@@ -14,6 +24,7 @@ class App extends React.Component {
     return (
       <div className="game-zone">
         <Snake snakeDots={this.state.snakeDots} />
+        <Food dot={this.state.food} />
       </div>
     );
   }
