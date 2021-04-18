c5279d3c92028d7126ab636319363ffbac9295a6
NjohPrince <jufredprince@gmail.com>
2021-04-18 11:01:18 +0100

    created the game area and snake

--- ReactProjects/snake-game/src/App.js
+++ ReactProjects/snake-game/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="game-zone">
-      
+      <div className="snake-dot" style={{top: 0, left: 0}}></div>
+      <div className="snake-dot" style={{top: 0, left: '2%'}}></div>
+      <div className="snake-dot" style={{top: 0, left: '4%'}}></div>
     </div>
   );
 }
